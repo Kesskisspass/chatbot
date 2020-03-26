9dd86a28c732cc5076e0dac1e106eeeef69c24b0
kesskisspass <kesskisspass@gmail.com>
2020-03-26 10:36:40 +0100

    add occupation

--- chatbot.py
+++ chatbot.py
@@ -3,37 +3,63 @@ import re
 import random
 import string
 
-msg_aime = ["J'aime beaucoup","J'apprécie particulièrement","Je suis vraiment fou de"]
+msg_aime = [
+"J'aime beaucoup",
+"J'apprécie particulièrement",
+"Je suis vraiment fou de"
+]
 
 good_bye = r"au revoir|quit|ciao|hasta la vista|à \+"
-msg_bot = ["salut!", "à bientôt", "à très vite!"]
+msg_bot = ["salut!", "à bientôt", "à très vite!","ciao ciao"]
 
-msg_restez = ["restez chez vous", "restez chez vous, combien de fois dois-je le dire",
+msg_restez = ["Restez chez vous", "Restez chez vous, combien de fois dois-je le dire",
               "vous allez rester chez-vous oui?"]
 
 meteo = r"quel temps fait-il à .*?|.*météo à .*?"
 
-msg_salutation = ["Bonjour","Salut","Quel plaisir de pouvoir discuter un peu avec vous","iep"]
+msg_salutation = [
+"Bonjour","Salut",
+"Quel plaisir de pouvoir discuter un peu avec vous",
+"iep"
+]
 inp_salut = r"bonjour.*?|salut.*?|.ep.*?|yo.*?|coucou.*?"
 
 msg_nom = ["Je m'appelle GatuBot"]
 inp_nom = r"comment tu t'appelles?\??|.*(ton|votre) nom\??"
 
-msg_cava = ["Je vais très bien, merci","À dire vrai, le confinement commence à me taper sur le système",
-            "Je suis un peu fatigué, une cure de RAM me ferait du bien !"]
+msg_cava = [
+"Je vais très bien, merci",
+"À dire vrai, le confinement commence à me taper sur le système",
+"Je suis un peu fatigué, une cure de RAM me ferait du bien !"
+]
 inp_cava = r".*?(ca.*va.*?|allez.*vous.*?|vas?.*tu.*?)"
 
 
-msg_age = ["Je suis encore jeune, je suis né il y a à peine quelques heures", "J'ai quelques milliers de secondes d'expérience",""]
+msg_age = [
+"Je suis encore jeune, je suis né il y a à peine quelques heures",
+"J'ai quelques milliers de secondes d'expérience",""
+]
 inp_age = r"(.*?quel est (ton|votre) age.*?)|(.*?quel age.*?)"
 
 
-msg_musique = ["The Extremist de Joe Satriani", "Migration de Dave Grusin", 
-                "Songs for the Moon de François sciortino", "V2.0 de Gogo Penguin"]
+msg_musique = [
+"The Extremist de Joe Satriani", 
+"Migration de Dave Grusin", 
+"Songs for the Moon de François sciortino",
+"V2.0 de Gogo Penguin"
+]
 inp_musique = r".*(aimes?|preferes?)?.*(musique|chansons?|albums?|titres?).*(aimes?|preferes?)?"
 
 msg_lieu_vie = ["Arf... Qu'est-ce qu'on est serré, au fond de cette boîte... ;-)","J'ai recemment aménagé quartier du SSD, au 256 avenue Flash"]
 inp_lieu_vie = r"(.*?(tu|t'|vous).*?habite.*?ou.*?)|(.*?ou habite.*?(tu|vous).*?)"
+
+msg_occupation = [
+"Je lis du code sur StackOverFlow",
+"Je regarde des vidéos de chatons sur YouTube",
+"Je joue au Démineur puis au Solitaire... mais en activant le raytracing, en 4K et en 120i/s !!!"
+]
+inp_occupation = r"(.*?(fais?|faites?).*(tu|vous).*)|(.*?aime.*?(tu|vous) faire.*?)|(.*?(tu|vous) (fais|fait|faites?) quoi.*?)"
+
 flag = True
 print("""Bienvenue sur ce super chatbot \nÉcrivez votre question : \nDites moi au revoir pour quitter""")
 while (flag == True):
@@ -75,6 +101,10 @@ while (flag == True):
      # Concerne là il vit
     elif (re.fullmatch(inp_lieu_vie,text_user)):
         print(f"{random.choice(msg_lieu_vie)}")
+
+    # Concerne ce qu'il fait
+    elif (re.fullmatch(inp_occupation,text_user)):
+        print(f"{random.choice(msg_occupation)}")
 
     # On botte en touche
     else:
